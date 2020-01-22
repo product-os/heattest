9e7b1018cea0defce6e7f84990fc0c0471ee60a0
Anuj Deshpande <anuj@makerville.io>
2020-01-22 14:07:36 +0530

    Cleanup old non-stressberry files

--- internaltemp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "date,temperature,clockspeed"
-while : ; do
-   echo "$(date +%s),$(vcgencmd measure_temp | sed "s/.*=\(.*\)'C/\1/"),$(vcgencmd measure_clock arm | sed "s/.*=\(.*\)/\1/")"
-   sleep 1
-done
