20fd884e7bbd28d6a3df73d6271b03a5ac7d5fff
Anuj Deshpande <anuj@makerville.io>
2020-01-22 14:12:12 +0530

    Add an improved plot command

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Starting stressberry ..."
 stressberry-run out.dat
-MPLBACKEND=Agg stressberry-plot out.dat -o out.png
+MPLBACKEND=Agg  stressberry-plot out.dat -f -d 300 -f -l 400 1600 -t 30 90 -o plot.png --hide-legend --not-transparent --line-width 2
 echo "Done stressberry"
 echo "Starting server"
 /usr/bin/python3 -m http.server
