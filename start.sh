c6b123493d4e94ee9524fd22790abeb5f8a67e8b
Anuj Deshpande <anuj@makerville.io>
2020-02-10 10:48:46 +0530

    Add flag for ambient temp sensor

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 echo "Starting stressberry ..."
-stressberry-run out.dat
+stressberry-run -v
+stressberry-run out.dat --duration 3600 --ambient 2302 23
 MPLBACKEND=Agg  stressberry-plot out.dat -f -d 300 -f -l 400 1600 -t 30 90 -o plot.png --hide-legend --not-transparent --line-width 2
 echo "Done stressberry"
 echo "Starting server"
-/usr/bin/python3 -m http.server
+/usr/bin/python3 -m http.server 80
