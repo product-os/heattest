b8f484daef327dd63f0a53f38f1bf62c5ad8b4b7
Anuj Deshpande <anuj@makerville.io>
2020-06-04 10:16:52 +0530

    Remove second service, get png csv from scope after every 80 deg measurement

--- heat-testing/start.sh
+++ heat-testing/start.sh
@@ -1,5 +1,6 @@
 echo "Starting stressberry ..."
 stressberry-run -v
+/usr/bin/python3 -m http.server 80&
 
 # stressberry-run out.dat --duration 3600 --ambient 2302 23 #--idle 10 --cooldown 10
 
@@ -11,4 +12,11 @@ MPLBACKEND=Agg  stressberry-plot out.dat -f -d 300 -f -l 400 1600 -t 30 90 -o pl
 # /usr/bin/python3 test/test_stressberry_standard.py
 echo "Done stressberry"
 echo "Starting server"
-/usr/bin/python3 -m http.server 80
+
+# Snipped to take oscilloscope readings every minute
+# while true
+# do
+#     python2 DS1054Z_screen_capture/OscScreenGrabLAN.py csv 192.168.1.5
+#     python2 DS1054Z_screen_capture/OscScreenGrabLAN.py png 192.168.1.5
+#     sleep 60 # wait for a minute
+# done
--- test-chamber/start.sh
+++
@@ -1,7 +0,0 @@
-python2 -m SimpleHTTPServer 8081&
-while true
-do
-    python2 DS1054Z_screen_capture/OscScreenGrabLAN.py csv 192.168.1.5
-    python2 DS1054Z_screen_capture/OscScreenGrabLAN.py png 192.168.1.5
-    sleep 60
-done
