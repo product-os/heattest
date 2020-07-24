2863271225ca9bcfd302a650bdf337d6851e5141
Anuj Deshpande <anuj@makerville.io>
2020-07-24 19:45:21 +0530

    Add logging to csv file
    - Also added CPU freq measurement
    - Remove printing USB devices. Only number of devices visible now
    Change-type: minor
    Signed-off-by: Anuj Deshpande <anuj@balena.io>

--- temp-measure/start.sh
+++ temp-measure/start.sh
@@ -1,2 +1,2 @@
+python3 -m http.server 80&
 python3 temp_measure.py
-python3 -m http.server 80
--- temp-measure/temp_measure.py
+++ temp-measure/temp_measure.py
@@ -6,46 +6,63 @@ from gpiozero import CPUTemperature
 import usb
 import usb.core
 import usb.util
+import subprocess
+import csv
+from table_logger import TableLogger
 
+timestr = time.strftime("%Y%m%d-%H%M%S")
+print (timestr)
+csvfile = open(timestr+".csv","wb")
+tbl = TableLogger(file=csvfile, csv = True,columns='Count,Time,CPU,USB,Freq,Ambient,Humidity')
 
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT22(board.D18)
 cpu = CPUTemperature()
-
+count = 0
 while True:
+        count = count + 1
         try:
             # Print the values to the serial port
             temperature_c = dhtDevice.temperature
-            temperature_f = temperature_c * (9 / 5) + 32
             humidity = dhtDevice.humidity
-            print(
-            str(datetime.datetime.now()).split('.')[0] +"| CPU "+str(cpu.temperature)+" | Ambient: {:.1f} F / {:.1f} C  |  Humidity: {}% ".format(
-                    temperature_f, temperature_c, humidity
-                )
-            )
+
             dev = usb.core.find(find_all=True)
 
             # get next item from the generator
+            sum = 0
             for d in dev:
+                sum = sum + 1
                 # print (d.get_active_configuration())
-                if d._manufacturer is None:
-                    try:
-                        d._manufacturer = usb.util.get_string(d, d.iManufacturer)
-                    except:
-                        print("Couldn't find manufacturer id")
-                    else:
-                        print (str(d._manufacturer))
-                if d._product is None:
-                    try:
-                        d._product = usb.util.get_string(d, d.iProduct)
-                    except:
-                        print("Couldn't find product id")
-                    else:        
-                        print (str(d._product))
-                print ("++++++++++++++++++++++")
-                
+                # if d._manufacturer is None:
+                #     try:
+                #         d._manufacturer = usb.util.get_string(d, d.iManufacturer)
+                #     except:
+                #         print("Couldn't find manufacturer id")
+                #     else:
+                #         print (str(d._manufacturer))
+                # if d._product is None:
+                #     try:
+                #         d._product = usb.util.get_string(d, d.iProduct)
+                #     except:
+                #         print("Couldn't find product id")
+                #     else:
+                #         print (str(d._product))
+                # print ("++++++++++++++++++++++")
+
         except RuntimeError as error:
             # Errors happen fairly often, DHT's are hard to read, just keep going
             print(error.args[0])
-        print("===================================")
+
+        out = subprocess.check_output(["vcgencmd", "measure_clock arm"]).decode("utf-8")
+        frequency = float(out.split("=")[1]) / 1000000
+        tbl(count, str( datetime.datetime.now()).split('.')[0] ,str(cpu.temperature),str(sum),str(round(frequency)),str(temperature_c),str(humidity))
+        print(
+                str( datetime.datetime.now()).split('.')[0]
+              +"|"+str(cpu.temperature)
+              +"|"+str(sum)
+              +"|"+str(round(frequency))
+              +"|"+str(temperature_c)
+              +"|"+str(humidity)
+        )
+        # print("===================================")
         time.sleep(2.0)
